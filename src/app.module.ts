29304e3249db4ebced07951a69225d93fb9b507c
E1tr <gastian02@gmail.com>
2025-10-13 19:07:12 +0200

    Intentar conectar la base de datos (Dani)

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,10 +8,23 @@ import { GrupoModule } from './grupo/grupo.module';
 import { NotificacionModule } from './notificacion/notificacion.module';
 import { ComunicacionModule } from './comunicacion/comunicacion.module';
 import { UsuarioModule } from './usuario/usuario.module';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
 @Module({
-  imports: [TrabajadorModule, TeleoperadorModule, SupervisorModule, GrupoModule, NotificacionModule, ComunicacionModule, UsuarioModule],
+  imports: [TrabajadorModule, TeleoperadorModule, SupervisorModule, GrupoModule, NotificacionModule, ComunicacionModule, UsuarioModule,
+    TypeOrmModule.forRoot({
+      type: 'mariadb',
+      host: process.env.DB_HOST,
+      port: 3306,
+      username: 'cuidemjunts_user',
+      password: 'elnegroeselimpostor',
+      database: 'cuidemjunts_db',
+      entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+      synchronize: false,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
+
+
 })
-export class AppModule {}
+export class AppModule { }
--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,10 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  console.log('Usuario:', process.env.DB_USER);
+  console.log('Contraseña:', process.env.DB_PASSWORD);
+
+
   // Permite conexiones desde fuera del contenedor (Docker)
   const port = process.env.PORT ?? 3000;
   await app.listen(port, '0.0.0.0');

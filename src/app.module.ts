51fc0f8af9a637b0a43c500c99c1dfe500620d99
E1tr <gastian02@gmail.com>
2025-10-16 16:22:14 +0200

    Intentar conectar el env con el código para que funcione la bd (Dani)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { Inject, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TrabajadorModule } from './trabajador/trabajador.module';
@@ -9,22 +9,39 @@ import { NotificacionModule } from './notificacion/notificacion.module';
 import { ComunicacionModule } from './comunicacion/comunicacion.module';
 import { UsuarioModule } from './usuario/usuario.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 @Module({
-  imports: [TrabajadorModule, TeleoperadorModule, SupervisorModule, GrupoModule, NotificacionModule, ComunicacionModule, UsuarioModule,
-    TypeOrmModule.forRoot({
-      type: 'mariadb',
-      host: process.env.DB_HOST,
-      port: 3306,
-      username: 'cuidemjunts_user',
-      password: 'elnegroeselimpostor',
-      database: 'cuidemjunts_db',
-      entities: [__dirname + '/../**/*.entity{.ts,.js}'],
-      synchronize: false,
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
     }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (config: ConfigService) => {
+        console.log('PROCESS ENV DB_USER=', process.env.DB_USER);
+        console.log('PROCESS ENV DB_PASSWORD present=', !!process.env.DB_PASSWORD);
+        return {
+          type: 'mysql',
+          host: config.get<string>('DB_HOST'),
+          port: Number(config.get<string>('DB_PORT') ?? 3306),
+          username: config.get<string>('DB_USER'),
+          password: config.get<string>('DB_PASSWORD'),
+          database: config.get<string>('DB_DATABASE'),
+          entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+          synchronize: false,
+        }
+      },
+      inject: [ConfigService],
+    }),
+    TrabajadorModule,
+    TeleoperadorModule,
+    SupervisorModule,
+    GrupoModule,
+    NotificacionModule,
+    ComunicacionModule,
+    UsuarioModule,
   ],
   controllers: [AppController],
   providers: [AppService],
-
-
 })
 export class AppModule { }

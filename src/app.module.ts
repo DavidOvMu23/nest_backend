599c1881d387b983c41707b22c91c9e61fa2aa3d
chverma <chverma@users.noreply.github.com>
2025-10-16 16:50:39 +0200

    Return to environment variables on main module

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,17 +9,18 @@ import { NotificacionModule } from './notificacion/notificacion.module';
 import { ComunicacionModule } from './comunicacion/comunicacion.module';
 import { UsuarioModule } from './usuario/usuario.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+
 @Module({
   imports: [TrabajadorModule, TeleoperadorModule, SupervisorModule, GrupoModule, NotificacionModule, ComunicacionModule, UsuarioModule,
     TypeOrmModule.forRoot({
       type: 'mariadb',
       host: process.env.DB_HOST,
       port: 3306,
-      username: 'cuidemjunts_user',
-      password: 'elnegroeselimpostor',
-      database: 'cuidemjunts_db',
-      entities: [__dirname + '/../**/*.entity{.ts,.js}'],
-      synchronize: false,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      entities: [__dirname + '/**/*.entity.ts'],
+      synchronize: true,
     }),
   ],
   controllers: [AppController],
